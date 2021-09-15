670926c739b27486f841d3402c964766b54e48f2
Philippe Dellaert <philippe@dellaert.org>
2021-09-15 15:24:55 -0700

    Auto generated from API Specifications for 20.10.6

---
+++ vspk/vspk/NSGMigrationProfile.cs
@@ -0,0 +1,209 @@
+/*
+  Copyright (c) 2017, Nokia
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of the copyright holder nor the names of its contributors
+        may be used to endorse or promote products derived from this software without
+        specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using net.nuagenetworks.bambou;
+
+using net.nuagenetworks.vspk.v6.fetchers;
+
+namespace net.nuagenetworks.vspk.v6
+{
+
+public class NSGMigrationProfile: RestObject {
+
+   private const long serialVersionUID = 1L;
+
+   
+   public enum EEntityScope {ENTERPRISE,GLOBAL };
+
+   
+   [JsonProperty("creationDate")]
+   protected String _creationDate;
+   
+   [JsonProperty("description")]
+   protected String _description;
+   
+   [JsonProperty("destinationProxyFQDN")]
+   protected String _destinationProxyFQDN;
+   [JsonConverter(typeof(StringEnumConverter))]
+   [JsonProperty("entityScope")]
+   protected EEntityScope? _entityScope;
+   
+   [JsonProperty("externalID")]
+   protected String _externalID;
+   
+   [JsonProperty("lastUpdatedBy")]
+   protected String _lastUpdatedBy;
+   
+   [JsonProperty("lastUpdatedDate")]
+   protected String _lastUpdatedDate;
+   
+   [JsonProperty("name")]
+   protected String _name;
+   
+   [JsonProperty("owner")]
+   protected String _owner;
+   
+
+   
+   [JsonIgnore]
+   private PermissionsFetcher _permissions;
+   
+   public NSGMigrationProfile() {
+      
+      _permissions = new PermissionsFetcher(this);
+      
+   }
+
+   
+   [JsonIgnore]
+   public String NUCreationDate {
+      get {
+         return _creationDate;
+      }
+      set {
+         this._creationDate = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NUDescription {
+      get {
+         return _description;
+      }
+      set {
+         this._description = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NUDestinationProxyFQDN {
+      get {
+         return _destinationProxyFQDN;
+      }
+      set {
+         this._destinationProxyFQDN = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public EEntityScope? NUEntityScope {
+      get {
+         return _entityScope;
+      }
+      set {
+         this._entityScope = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NUExternalID {
+      get {
+         return _externalID;
+      }
+      set {
+         this._externalID = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NULastUpdatedBy {
+      get {
+         return _lastUpdatedBy;
+      }
+      set {
+         this._lastUpdatedBy = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NULastUpdatedDate {
+      get {
+         return _lastUpdatedDate;
+      }
+      set {
+         this._lastUpdatedDate = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NUName {
+      get {
+         return _name;
+      }
+      set {
+         this._name = value;
+      }
+   }
+
+   
+   [JsonIgnore]
+   public String NUOwner {
+      get {
+         return _owner;
+      }
+      set {
+         this._owner = value;
+      }
+   }
+
+   
+
+   
+   public PermissionsFetcher getPermissions() {
+      return _permissions;
+   }
+   
+
+   public String toString() {
+      return "NSGMigrationProfile [" + "creationDate=" + _creationDate + ", description=" + _description + ", destinationProxyFQDN=" + _destinationProxyFQDN + ", entityScope=" + _entityScope + ", externalID=" + _externalID + ", lastUpdatedBy=" + _lastUpdatedBy + ", lastUpdatedDate=" + _lastUpdatedDate + ", name=" + _name + ", owner=" + _owner + ", id=" + NUId + ", parentId=" + NUParentId + ", parentType=" + NUParentType  + "]";
+   }
+   
+   
+
+   public static String getResourceName()
+   {
+	return "nsgmigrationprofiles";
+   }
+
+   public static String getRestName()
+   {
+	return "nsgmigrationprofile";
+   }
+}
+}
